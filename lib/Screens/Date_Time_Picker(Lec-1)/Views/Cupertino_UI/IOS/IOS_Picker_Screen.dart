25af1f2956e5984347e99b24a5ace78329f5e2de
Admin <maayush235@gmail.com>
2024-05-05 23:46:41 +0530

    Radhey Radhey

--- lib/Screens/Date_Time_Picker(Lec-1)/Views/Cupertino_UI/IOS/IOS_Picker_Screen.dart
+++ lib/Screens/Date_Time_Picker(Lec-1)/Views/Cupertino_UI/IOS/IOS_Picker_Screen.dart
@@ -49,13 +49,14 @@ class Ios_Pickers extends StatelessWidget {
                   },
                   icon: Icon(Icons.date_range),
                 ),
-
-                Text('Date',style: TextStyle(
-                  fontSize: 30,
-                ),)
+                Text(
+                  'Date',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )
               ],
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -66,23 +67,25 @@ class Ios_Pickers extends StatelessWidget {
                       builder: (BuildContext builder) {
                         return Container(
                           height: 200,
-                          child: CupertinoTimerPicker(onTimerDurationChanged: (value) {
-                                (Duration value){
-                            };
-                          },),
+                          child: CupertinoTimerPicker(
+                            onTimerDurationChanged: (value) {
+                              (Duration value) {};
+                            },
+                          ),
                         );
                       },
                     );
                   },
                   icon: Icon(Icons.timer_outlined),
                 ),
-
-                Text('Time',style: TextStyle(
-                  fontSize: 30,
-                ),)
+                Text(
+                  'Time',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )
               ],
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -91,12 +94,74 @@ class Ios_Pickers extends StatelessWidget {
                     showCupertinoModalPopup(
                       context: context,
                       builder: (BuildContext builder) {
-                        return Container(
-                          height: 200,
-                          child: CupertinoDatePicker(
-                            mode: CupertinoDatePickerMode.date,
-                            initialDateTime: DateTime.now(),
-                            onDateTimeChanged: (DateTime newDateTime) {},
+                        return CupertinoActionSheet(
+                          title: Text(
+                            'Favorite Dessert',
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.w500),
+                          ),
+                          message: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 14),
+                            child: Text(
+                              'Please select the best dessert from the\noptions below.',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          actions: [
+                            CupertinoActionSheetAction(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                'Profiteroles',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w400,
+                                letterSpacing: 1),
+                              ),
+                            ),
+
+                            CupertinoActionSheetAction(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                'Cannolis',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w400,
+                                letterSpacing: 1),
+                              ),
+                            ),
+
+                            CupertinoActionSheetAction(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                'Trifle',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w400,
+                                letterSpacing: 1),
+                              ),
+                            ),
+                          ],
+                          cancelButton: CupertinoActionSheetAction(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            isDefaultAction: true,
+                            child: const Text(
+                              'Cancel',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 23,
+                                  fontWeight: FontWeight.w600),
+                            ),
                           ),
                         );
                       },
@@ -104,10 +169,12 @@ class Ios_Pickers extends StatelessWidget {
                   },
                   icon: Icon(Icons.date_range),
                 ),
-
-                Text('Action Sheet',style: TextStyle(
-                  fontSize: 30,
-                ),)
+                Text(
+                  'Action Sheet',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )
               ],
             ),
           ],
