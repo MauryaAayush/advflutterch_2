c52f77eed15f956bb1d7772b872841853868f0c6
Admin <maayush235@gmail.com>
2024-05-15 09:55:58 +0530

    Radhey Radhey

--- lib/Screens/PageView(lec-6)/Views/home_Screen.dart
+++ lib/Screens/PageView(lec-6)/Views/home_Screen.dart
@@ -1,16 +1,34 @@
+import 'package:advflutterch_2/Screens/TabBar,%20Slider,%20SliderSegmentControl(lec-5)/Provider/Tac_Screen_Provider.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Screen',
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),),
-      ),
+    return Scaffold(bottomNavigationBar: AnimatedBottomNavigationBar(
+      icons: [
+      Icons.home,
+      Icons.search_rounded,
+      Icons.add_box_outlined,
+      Icons.favorite_border
+      ],
+
+
+      notchSmoothness: NotchSmoothness.defaultEdge,
+      backgroundColor: Colors.grey,
+      splashColor: Colors.black,
+      activeIndex: Provider.of<TabBarScreen>(context,listen: true).index,
+      onTap:(value) {
+        Provider.of<TabBarScreen>(context,listen: false).ChangedIndex(value.toString());
+      },
+      //other params
+    )
     );
+
+
   }
 }
 
