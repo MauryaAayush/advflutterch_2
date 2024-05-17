99e72f7ce51a5e0a28d3321d8e6efccbec226349
Admin <maayush235@gmail.com>
2024-05-17 11:19:03 +0530

    Radhey Radhey

--- lib/Screens/DatePicker(lec-8)/Views/Material_DatePicker_Screen.dart
+++ lib/Screens/DatePicker(lec-8)/Views/Material_DatePicker_Screen.dart
@@ -19,35 +19,38 @@ class Date_Picker_Screen extends StatelessWidget {
         child: Column(
           children: [
 
-            TextField(
-              controller: dateController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.black, width: 1.5),
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () async {
-                    DateTime selectedDate = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime(1947),
-                      lastDate: DateTime(2047),
-                      initialDate: DateTime.now(),
-                    ) ??
-                        DateTime.now();
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: TextField(
+                controller: dateController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.black, width: 1.5),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () async {
+                      DateTime selectedDate = await showDatePicker(
+                        context: context,
+                        firstDate: DateTime(1947),
+                        lastDate: DateTime(2047),
+                        initialDate: DateTime.now(),
+                      ) ??
+                          DateTime.now();
 
-                    String formattedDate =
-                        "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}";
-                    dateController.text = formattedDate;
+                      String formattedDate =
+                          "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}";
+                      dateController.text = formattedDate;
 
-                    print(selectedDate);
-                  },
-                  icon: Icon(Icons.calendar_month),
+                      print(selectedDate);
+                    },
+                    icon: Icon(Icons.calendar_month),
+                  ),
+                  hintText: 'dd/mm/yyy',
+                  labelText: 'Date',
                 ),
-                hintText: 'dd/mm/yyy',
-                labelText: 'Date',
+                readOnly: true,
               ),
-              readOnly: true,
             ),
             // IconButton(
             //     onPressed: () async {
